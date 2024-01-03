07868c1d0f7ca9058f1838d62f9046693269f95f
Gary H <3rdeye.ghj@gmail.com>
2024-01-03 11:47:17 -0800

    Adding nested routes for homepage

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 import { RouterProvider } from 'react-router-dom';
-import { Router, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = createBrowserRouter(createRoutesFromElements(<Route path='/' element={ <Root/> }/>));
+const appRouter = createBrowserRouter(createRoutesFromElements(
+<Route path='/' element={ <Root/> }>
+  <Route index element='/pages/home'/>
+</Route>));
 
 function App() {
   return (
